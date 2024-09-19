118abb283955d628eb952c3a2afcc16362e3e9f6
Kunal-rajore <146552231+Kunal-rajore@users.noreply.github.com>
2024-09-19 12:24:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+let input = document.querySelector("input");
+
+Array.from(buttons).forEach((button) => {
+button.addEventListener("click",(e)=>{
+   if(e.target.innerHTML == "="){
+    string = eval(string);
+    input.value = string;
+   } else if(e.target.innerHTML == "AC"){
+    string = "";
+    input.value = string;
+   }else if(e.target.innerHTML == "del"){
+     string = string.slice(0,-1);
+     input.value = string;
+   }else{
+    string = string + e.target.innerHTML;
+    input.value = string;
+   }
+  
+    
+})
+});
